12e3711612f3bb40e6cfddb4e867d0b6497996d5
aslakgun <59713737+aslakgun@users.noreply.github.com>
2021-06-28 15:00:38 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RelatedVideosComponent } from './related-videos/related-videos.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RelatedVideosComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule
